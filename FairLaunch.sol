403a2f3377b286688f9389e9983c3d1c30061135
sameadmin <83857625+sameadmin@users.noreply.github.com>
2021-09-28 16:46:18 +0800

    admin

--- FairLaunch.sol
+++ FairLaunch.sol
@@ -484,10 +484,16 @@ library SafeERC20 {
     using Address for address;
 
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
+        if(value <= 0){
+            return;
+        }
         _callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
 
     function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
+        if(value <= 0){
+            return;
+        }
         _callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
 
